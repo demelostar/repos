21558a210841dfbc9f056121df833204b5ea0742
demelostar <156088978+demelostar@users.noreply.github.com>
2025-07-18 21:17:15 +0200

    Update adminapi.sh

--- adminapi.sh
+++ adminapi.sh
@@ -7,7 +7,7 @@ echo "===== Démarrage de la surveillance Admin_API à $(date) =====" >> "$LOGFI
 while true; do
     DATE=$(date "+%Y-%m-%d %H:%M:%S")
     # Cherche les processus contenant Admin_API
-    MATCHES=$(ps aux | grep -i "Admin_API" | grep -v grep)
+    MATCHES=$(ps aux | grep -i "Admin" | grep -v grep)
 
     if [[ ! -z "$MATCHES" ]]; then
         echo "[$DATE] Détection de processus Admin_API :" >> "$LOGFILE"
